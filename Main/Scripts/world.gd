a47678c5967c18fab769b37c57b597650e6d1a0e
BigCheese236 <bigcheesecompany236@gmail.com>
2025-07-20 01:45:03 +0200

    visual feedback

--- Main/Scripts/world.gd
+++ Main/Scripts/world.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 
 @onready var won: Label = $UI/won
+@onready var canvas_modulate: CanvasModulate = $CanvasModulate
 
 func _ready() -> void:
 	won.hide()
@@ -12,3 +13,10 @@ func _process(_delta: float) -> void:
 	if Global.hit:
 		won.show()
 		Global.hit = false
+
+	if Global.current_sense == "Sight":
+		canvas_modulate.color = Color(0.08, 0.09, 0.15, 1.0)
+	elif Global.current_sense == "Touch":
+		canvas_modulate.color = Color(0.12, 0.07, 0.07, 1.0)
+	else:
+		canvas_modulate.color = Color(0.133, 0.133, 0.133, 1.0)
