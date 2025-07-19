23c5858e55e559e61bc2f202ea6f57bb992510b9
BigCheese236 <bigcheesecompany236@gmail.com>
2025-07-19 16:05:07 +0200

    Player setup

---
+++ Player/Scripts/cam.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+@export var randomStrength: float = 10.0
+@export var shakeFade: float = 15.0
+
+var rng = RandomNumberGenerator.new()
+
+var shake_strength: float = 0.0
+
+func _process(delta: float) -> void:
+	if shake_strength > 0:
+		shake_strength = lerpf(shake_strength,0,shakeFade * delta)
+		
+		offset = randomOffset()
+
+# Call this function to let the screen shake when the player shoots or something
+func apply_shake():
+	shake_strength = randomStrength
+
+func randomOffset() -> Vector2:
+	return Vector2(rng.randf_range(-shake_strength,shake_strength),rng.randf_range(-shake_strength,shake_strength))
--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+
+var speed: float = 300.0
+
+@onready var cam: Camera2D = $cam
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		cam.apply_shake()
+
+func _ready() -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	var input_vector: Vector2 = Vector2(Input.get_axis("move_left", "move_right"),Input.get_axis("move_up", "move_down")).normalized()
+	velocity = input_vector * speed
+	move_and_slide()
