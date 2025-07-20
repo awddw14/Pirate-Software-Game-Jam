bea209253c955476fff7ad6f16eba00cea68c393
BigCheese236 <bigcheesecompany236@gmail.com>
2025-07-21 01:02:00 +0200

    Placeholder

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -10,7 +10,12 @@ var gun_equip: bool = false
 @onready var light: PointLight2D = $PointLight2D
 @onready var weapon: Sprite2D = $weapon
 @onready var ap: AnimationPlayer = $ap
+@onready var anim: AnimatedSprite2D = $anim
+@onready var crosshair: Sprite2D = $Crosshair
+
 var is_sight: bool = false
+var target = null
+var crosshair_playing := false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") and gun_equip:
@@ -28,14 +33,25 @@ func _process(_delta: float) -> void:
 	if gun_equip:
 		var mouse_pos = cam.get_global_mouse_position()
 		weapon.look_at(mouse_pos)
-		if mouse_pos.x < global_position.x:
-			weapon.flip_v = true
-		else :
-			weapon.flip_v = false
+		weapon.flip_v = mouse_pos.x < global_position.x
+		
+	if gun_equip and target != null:
+		crosshair.global_position = target.global_position
+		
+		if not crosshair_playing and target != null:
+			play_crosshair_anim()
 
 func _physics_process(_delta: float) -> void:
 	var input_vector: Vector2 = Vector2(Input.get_axis("move_left", "move_right"),Input.get_axis("move_up", "move_down")).normalized()
 	velocity = input_vector * speed
+	if input_vector.x < 0 or input_vector.y < 0:
+		anim.flip_h = true
+		anim.play("Walk")
+	elif input_vector.x > 0 or input_vector.y > 0:
+		anim.flip_h = false
+		anim.play("Walk")
+	elif velocity == Vector2(0,0):
+		anim.play("Idle")
 	move_and_slide()
 
 func update_sense():
@@ -64,10 +80,24 @@ func equip_weapon():
 	weapon.show()
 	gun_equip = true
 
+func play_crosshair_anim():
+	if not gun_equip or target == null:
+		return
+	crosshair_playing = true
+	crosshair.show()
+	ap.play("Crosshair_anim")
+	await ap.animation_finished
+	ap.play_backwards("Crosshair_anim")
+	await ap.animation_finished
+	crosshair_playing = false
+
+
 func _on_touch_sense_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Monster"):
 		Global.temp_change = true
+		target = body
 
 func _on_touch_sense_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Monster"):
 		Global.temp_change = false
+		crosshair.hide()
